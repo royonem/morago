0bd590f4e46fb7bf7bfc07a6cc1a05ecd6f76a9f
royonem <roywmyung@gmail.com>
2026-04-21 17:47:38 +0900

    chore: add Lombok to BaseEntity

--- src/main/java/com/roy/morago/entity/BaseEntity.java
+++ src/main/java/com/roy/morago/entity/BaseEntity.java
@@ -1,12 +1,20 @@
 package com.roy.morago.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
