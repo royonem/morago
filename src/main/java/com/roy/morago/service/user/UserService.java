283f9b217808760c0720baabca9efc4da63873b4
royonem <roywmyung@gmail.com>
2026-04-27 16:49:32 +0900

    chore(user): remove createUser methods from UserService (to be moved to auth service)

--- src/main/java/com/roy/morago/service/user/UserService.java
+++ src/main/java/com/roy/morago/service/user/UserService.java
@@ -1,7 +1,5 @@
 package com.roy.morago.service.user;
 
-import com.roy.morago.dto.auth.ClientRegisterRequest;
-import com.roy.morago.dto.auth.TranslatorRegisterRequest;
 import com.roy.morago.dto.user.UpdateUserRequest;
 import com.roy.morago.dto.user.UserResponse;
 import com.roy.morago.entity.user.Language;
@@ -22,20 +20,14 @@ public class UserService {
     private final LanguageRepository languageRepository;
     private final UserMapper userMapper;
 
-    public void createUser(ClientRegisterRequest userDto) {
-        if (userRepository.existsByEmail(userDto.getEmail())) {
-            throw new RuntimeException("Email already in use");
-        }
-        User user = userMapper.createUserFromDto(userDto);
-        userRepository.save(user);
+    public UserResponse getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return userMapper.toUserResponse(user);
     }
 
-    public void createUser(TranslatorRegisterRequest userDto) {
-        if (userRepository.existsByEmail(userDto.getEmail())) {
-            throw new RuntimeException("Email already in use");
-        }
-        User user = userMapper.createUserFromDto(userDto);
-        userRepository.save(user);
+    public List<UserResponse> getAllUsers() {
+        return userMapper.toUserResponse(userRepository.findAll());
     }
 
     @Transactional
@@ -49,16 +41,6 @@ public class UserService {
             user.setLanguages(languages);
         }
         userRepository.save(user);
-    }
-
-    public List<UserResponse> getAllUsers() {
-        return userMapper.toUserResponse(userRepository.findAll());
-    }
-
-    public UserResponse getUserById(Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        return userMapper.toUserResponse(user);
     }
 
     public void deleteUser(Long id) {
