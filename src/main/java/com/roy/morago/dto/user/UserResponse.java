520db35ed5b9eced5783ad4811bd18787520f0fa
royonem <roywmyung@gmail.com>
2026-04-27 13:54:32 +0900

    feat(user): add User Response DTO

---
+++ src/main/java/com/roy/morago/dto/user/UserResponse.java
@@ -0,0 +1,25 @@
+package com.roy.morago.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String profilePictureUrl;
+    private Set<String> roles;
+    private Set<String> languages;
+    private String availability;
+    private String status;
+    private Integer topikLevel;
+}
