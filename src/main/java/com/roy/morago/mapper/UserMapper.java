8dadb1d8dc2baf2ad01ee58f9a25f7396fba2c5e
royonem <roywmyung@gmail.com>
2026-04-30 16:03:35 +0900

    chore: add explicit mapping for fields in UserMapper

--- src/main/java/com/roy/morago/mapper/UserMapper.java
+++ src/main/java/com/roy/morago/mapper/UserMapper.java
@@ -4,12 +4,15 @@ import com.roy.morago.dto.auth.ClientRegisterRequest;
 import com.roy.morago.dto.auth.TranslatorRegisterRequest;
 import com.roy.morago.dto.user.UpdateUserRequest;
 import com.roy.morago.dto.user.UserResponse;
+import com.roy.morago.entity.user.Language;
+import com.roy.morago.entity.user.Role;
 import com.roy.morago.entity.user.User;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import com.roy.morago.repository.user.LanguageRepository;
+import org.mapstruct.*;
+
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -19,9 +22,27 @@ public interface UserMapper {
     User createUserFromDto(TranslatorRegisterRequest dto);
     // READ
     List<UserResponse> toUserResponse(List<User> users);
+
+    @Mapping(target = "roles", source = "roles")
+    @Mapping(target = "languages", source = "languages")
     UserResponse toUserResponse(User user);
     // UPDATE
+    @Mapping(target = "languages", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateUserFromDto(UpdateUserRequest dto, @MappingTarget User user);
+
+    default Set<String> mapRoles(Set<Role> roles) {
+        return roles == null ? Set.of() :
+                roles.stream()
+                        .map(Role::getName)
+                        .collect(Collectors.toSet());
+    }
+
+    default Set<String> mapLanguages(Set<Language> languages) {
+        return languages == null ? Set.of() :
+                languages.stream()
+                        .map(Language::getName)
+                        .collect(Collectors.toSet());
+    }
 
 }
