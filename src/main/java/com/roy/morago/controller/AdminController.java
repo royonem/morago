e1184e8108b2c3b8f9d977ae8b0dc66548e7166b
royonem <roywmyung@gmail.com>
2026-04-30 16:52:21 +0900

    chore: add PreAuthorize annotations for admin controller

--- src/main/java/com/roy/morago/controller/AdminController.java
+++ src/main/java/com/roy/morago/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.roy.morago.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -16,21 +17,25 @@ public class AdminController {
 
     private final UserService userService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/users")
     public List<UserResponse> userList() {
         return userService.getAllUsers();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/users/{id}")
     public UserResponse getUser(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/users/{id}")
     public void updateUser(@PathVariable Long id, @Valid @RequestBody UpdateUserRequest dto) {
         userService.updateUser(id, dto);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/users/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable Long id) {
