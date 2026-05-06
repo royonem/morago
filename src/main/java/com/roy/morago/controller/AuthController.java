921f534248ea25ead8841f82f0921c007ac9e5b2
royonem <roywmyung@gmail.com>
2026-05-06 14:25:04 +0900

    feat(auth): create AuthController and add login and registration methods

---
+++ src/main/java/com/roy/morago/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.roy.morago.controller;
+
+import com.roy.morago.dto.auth.ClientRegisterRequest;
+import com.roy.morago.dto.auth.LoginRequest;
+import com.roy.morago.dto.auth.LoginResponse;
+import com.roy.morago.dto.auth.TranslatorRegisterRequest;
+import com.roy.morago.service.auth.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+
+    @PostMapping("/register/client")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerClient(@Valid @RequestBody ClientRegisterRequest dto) {
+        authService.registerClient(dto);
+    }
+
+    @PostMapping("/register/translator")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerTranslator(@Valid @RequestBody TranslatorRegisterRequest dto) {
+        authService.registerTranslator(dto);
+    }
+}
