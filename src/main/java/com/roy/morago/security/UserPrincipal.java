bc8f752d15470014d192805c3b500795ae4dda0a
royonem <roywmyung@gmail.com>
2026-05-06 13:43:10 +0900

    feat(auth): add UserPrincipal class

---
+++ src/main/java/com/roy/morago/security/UserPrincipal.java
@@ -0,0 +1,52 @@
+package com.roy.morago.security;
+
+import com.roy.morago.entity.user.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
