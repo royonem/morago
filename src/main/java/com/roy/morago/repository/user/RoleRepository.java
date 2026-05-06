33a75be38c9703ab1e19e7714cc59523bdd1d402
royonem <roywmyung@gmail.com>
2026-05-06 13:52:33 +0900

    feat(role): add role repository method

--- src/main/java/com/roy/morago/repository/user/RoleRepository.java
+++ src/main/java/com/roy/morago/repository/user/RoleRepository.java
@@ -2,6 +2,8 @@ package com.roy.morago.repository.user;
 
 import com.roy.morago.entity.user.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String roleName);
 }
