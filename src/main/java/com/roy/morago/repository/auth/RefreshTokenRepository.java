f9bfedc69cde6648e0f2b05178219452de6969c9
royonem <roywmyung@gmail.com>
2026-05-11 10:30:11 +0900

    feat(auth): support refresh token lookup by token

--- src/main/java/com/roy/morago/repository/auth/RefreshTokenRepository.java
+++ src/main/java/com/roy/morago/repository/auth/RefreshTokenRepository.java
@@ -4,4 +4,5 @@ import com.roy.morago.entity.auth.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    RefreshToken findByToken(String token);
 }
