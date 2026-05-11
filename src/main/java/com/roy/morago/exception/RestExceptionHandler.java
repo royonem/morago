9074261308c2c5fb1235306f4d4e1e3d98915e0d
royonem <roywmyung@gmail.com>
2026-05-11 10:31:37 +0900

    feat(auth): add Invalid Refresh Token Exception and update exception handler

--- src/main/java/com/roy/morago/exception/RestExceptionHandler.java
+++ src/main/java/com/roy/morago/exception/RestExceptionHandler.java
@@ -37,4 +37,11 @@ public class RestExceptionHandler {
         response.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
+
+    @ExceptionHandler(InvalidRefreshTokenException.class)
+    public ResponseEntity<Map<String, String>> handleAll(InvalidRefreshTokenException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
 }
