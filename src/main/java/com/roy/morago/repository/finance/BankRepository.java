14179db1b7d47b705dd53f4f984286c01bfa265d
royonem <roywmyung@gmail.com>
2026-05-11 12:18:23 +0900

    feat(finance): add BankAccount DTO, Repository, and Service classes

---
+++ src/main/java/com/roy/morago/repository/finance/BankRepository.java
@@ -0,0 +1,7 @@
+package com.roy.morago.repository.finance;
+
+import com.roy.morago.entity.finance.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankRepository extends JpaRepository<BankAccount, Long> {
+}
---
+++ src/main/java/com/roy/morago/service/finance/BankService.java
@@ -0,0 +1,35 @@
+package com.roy.morago.service.finance;
+
+import com.roy.morago.dto.finance.BankAccountDTO;
+import com.roy.morago.entity.finance.BankAccount;
+import com.roy.morago.entity.user.User;
+import com.roy.morago.exception.BankNotFoundException;
+import com.roy.morago.repository.finance.BankRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BankService {
+    private final BankRepository bankRepository;
+
+    @Transactional
+    public BankAccount linkBankAccount(BankAccountDTO dto, User user) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setUser(user);
+        bankAccount.setBankName(dto.getBankName());
+        bankAccount.setAccountNumber(dto.getAccountNumber());
+        return bankRepository.save(bankAccount);
+    }
+
+    @Transactional
+    public void unlinkBankAccount(Long id) {
+        if (bankRepository.existsById(id)) {
+            bankRepository.deleteById(id);
+        } else {
+            throw new BankNotFoundException("Bank account not found");
+        }
+    }
+}
