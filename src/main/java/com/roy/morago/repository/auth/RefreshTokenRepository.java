525ed05b933ac284052018f8e4cbd47f37432ffd
royonem <roywmyung@gmail.com>
2026-05-11 09:31:54 +0900

    feat(auth): add RefreshToken Repository

---
+++ src/main/java/com/roy/morago/repository/auth/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package com.roy.morago.repository.auth;
+
+import com.roy.morago.entity.auth.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+}
