f9037dec181e9fafb40de9187ed172a369ed3c1d
royonem <roywmyung@gmail.com>
2026-05-12 11:21:39 +0900

    feat(security): add custom UserDetailsService to wire UserPrincipal

--- src/main/java/com/roy/morago/repository/user/UserRepository.java
+++ src/main/java/com/roy/morago/repository/user/UserRepository.java
@@ -2,7 +2,9 @@ package com.roy.morago.repository.user;
 
 import com.roy.morago.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
