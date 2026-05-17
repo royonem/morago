189699ea86f42f5f300f44426dcfeb6b9b51784f
royonem <roywmyung@gmail.com>
2026-05-17 13:45:25 +0900

    feat(dto): separated dtos based on input and output and added constraints

--- src/main/java/com/roy/morago/dto/finance/TransactionDTO.java
+++ src/main/java/com/roy/morago/dto/finance/TransactionDTO.java
@@ -2,6 +2,7 @@ package com.roy.morago.dto.finance;
 
 import com.roy.morago.enums.CurrencyCode;
 import com.roy.morago.enums.TransactionType;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -9,8 +10,12 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransactionDTO {
+    @NotNull(message = "Transaction type must be specified")
     private TransactionType type;
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be positive")
     private Long amount;
+    @NotNull(message = "Currency type must be specified")
     private CurrencyCode currencyCode;
     private String reference;
     private String description;
