dee8905a3399beddc245c44e1675ba90903f31f4
royonem <roywmyung@gmail.com>
2026-05-17 14:41:05 +0900

    feat(finance): add repository method for finding transactions by walletUserId and status

--- src/main/java/com/roy/morago/repository/finance/TransactionRepository.java
+++ src/main/java/com/roy/morago/repository/finance/TransactionRepository.java
@@ -1,7 +1,9 @@
 package com.roy.morago.repository.finance;
 
 import com.roy.morago.entity.finance.Transaction;
+import com.roy.morago.enums.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    boolean existsByWalletUserIdAndStatus(Long userId, TransactionStatus status);
 }
