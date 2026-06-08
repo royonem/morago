fc4c9ed827050dc64ee4c7dc3ca2e329fdf0acab
royonem <roywmyung@gmail.com>
2026-06-08 14:22:12 +0900

    feat(file): add findByUserId and findByTopicId to FileRepository

--- src/main/java/com/roy/morago/repository/file/FileRepository.java
+++ src/main/java/com/roy/morago/repository/file/FileRepository.java
@@ -3,5 +3,10 @@ package com.roy.morago.repository.file;
 import com.roy.morago.entity.file.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FileRepository extends JpaRepository<File, Long> {
+    Optional<File> findByUserId(Long id);
+
+    Optional<File> findByTopicId(Long id);
 }
