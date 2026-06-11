fd2efca39c1e0a40240e9a27f9bf2b3d858920a2
royonem <roywmyung@gmail.com>
2026-06-11 10:57:41 +0900

    feat(call): add CallNotFound exception

---
+++ src/main/java/com/roy/morago/exception/CallNotFoundException.java
@@ -0,0 +1,7 @@
+package com.roy.morago.exception;
+
+public class CallNotFoundException extends RuntimeException {
+    public CallNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/roy/morago/exception/RestExceptionHandler.java
+++ src/main/java/com/roy/morago/exception/RestExceptionHandler.java
@@ -216,4 +216,11 @@ public class RestExceptionHandler {
         response.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
+
+    @ExceptionHandler(CallNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleCallNotFound(CallNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 }
