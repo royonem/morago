5fe11ad2cfbd3df7cce1480ea859664a11d28cf3
royonem <roywmyung@gmail.com>
2026-06-11 17:48:19 +0900

    feat(auth): add getter to UserPrincipal

--- src/main/java/com/roy/morago/security/UserPrincipal.java
+++ src/main/java/com/roy/morago/security/UserPrincipal.java
@@ -2,11 +2,14 @@ package com.roy.morago.security;
 
 import com.roy.morago.entity.user.User;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
+@SuppressWarnings("ClassCanBeRecord")
+@Getter
 @AllArgsConstructor
 public class UserPrincipal implements UserDetails {
 
