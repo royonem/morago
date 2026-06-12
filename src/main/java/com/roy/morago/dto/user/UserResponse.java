640cc968be7d4ecb18a8e3d88525d167a342e8a2
royonem <roywmyung@gmail.com>
2026-06-12 13:09:57 +0900

    refactor(user): change UserResponse to record

--- src/main/java/com/roy/morago/dto/user/UserResponse.java
+++ src/main/java/com/roy/morago/dto/user/UserResponse.java
@@ -3,26 +3,18 @@ package com.roy.morago.dto.user;
 import com.roy.morago.enums.Availability;
 import com.roy.morago.enums.TopikLevel;
 import com.roy.morago.enums.UserStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.Set;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserResponse {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String profilePictureUrl;
-    private Set<String> roles;
-    private Set<String> languages;
-    private Availability availability;
-    private UserStatus status;
-    private TopikLevel topikLevel;
-}
+public record UserResponse(
+        Long id,
+        String firstName,
+        String lastName,
+        String email,
+        String profilePictureUrl,
+        Set<String> roles,
+        Set<String> languages,
+        Availability availability,
+        UserStatus status,
+        TopikLevel topikLevel
+) { }
