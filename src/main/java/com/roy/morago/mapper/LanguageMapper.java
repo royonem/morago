eb5209f7e5ace8e5a342a9b32ce33e046e176d3d
royonem <roywmyung@gmail.com>
2026-06-15 12:51:28 +0900

    fix: suppress unmapped target warnings in LanguageMapper

--- src/main/java/com/roy/morago/mapper/LanguageMapper.java
+++ src/main/java/com/roy/morago/mapper/LanguageMapper.java
@@ -4,10 +4,11 @@ import com.roy.morago.dto.user.LanguageRequest;
 import com.roy.morago.dto.user.LanguageResponse;
 import com.roy.morago.entity.user.Language;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface LanguageMapper {
     LanguageResponse toLanguageResponse(Language language);
     List<LanguageResponse> toLanguageResponseList(List<Language> languages);
