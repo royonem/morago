e7a0c99597c72da3bb03c09ec4c16e2434ea82c5
royonem <roywmyung@gmail.com>
2026-06-17 14:28:42 +0900

    refactor(auth): improve generateToken method structure

--- src/main/java/com/roy/morago/security/JwtProvider.java
+++ src/main/java/com/roy/morago/security/JwtProvider.java
@@ -1,12 +1,16 @@
 package com.roy.morago.security;
 
+import com.roy.morago.entity.user.Role;
+import com.roy.morago.entity.user.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
@@ -19,9 +23,13 @@ public class JwtProvider {
         this.jwtSigningKey = jwtSigningKey;
     }
 
-    public String generateToken(String username, Set<String> roleNames) {
+    public String generateToken(User user) {
+        String email = user.getEmail();
+        Set<String> roleNames = user.getRoles().stream()
+                .map(Role::getName)
+                .collect(Collectors.toSet());
         return Jwts.builder()
-                .subject(username)
+                .subject(email)
                 .claim("roleNames", roleNames)
                 .issuer("morago-backend")
                 .audience().add("morago-app")
