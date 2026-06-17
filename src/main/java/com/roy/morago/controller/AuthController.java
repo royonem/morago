6332a85421465c6a24f23f7f6650158519f500ca
royonem <roywmyung@gmail.com>
2026-06-17 14:34:06 +0900

    feat(auth): added logout and refresh logic and cleaned up structure of existing code

--- src/main/java/com/roy/morago/controller/AuthController.java
+++ src/main/java/com/roy/morago/controller/AuthController.java
@@ -1,13 +1,12 @@
 package com.roy.morago.controller;
 
-import com.roy.morago.dto.auth.RegisterClientRequest;
-import com.roy.morago.dto.auth.LoginRequest;
-import com.roy.morago.dto.auth.LoginResponse;
-import com.roy.morago.dto.auth.RegisterTranslatorRequest;
+import com.roy.morago.dto.auth.*;
 import com.roy.morago.service.auth.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -17,8 +16,21 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody LoginRequest request) {
+    public LoginResponse login(@Valid @RequestBody LoginRequest request) {
         return authService.login(request);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(@Valid @RequestBody LogoutRequest logoutRequest, Authentication authentication) {
+        authService.logout(logoutRequest, authentication);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/refresh")
+    public LoginResponse refresh(@Valid @RequestBody RefreshRequest request) {
+        return authService.refresh(request);
     }
 
     @PostMapping("/register/client")
