22ae8413fc66dd7f916a5491ce29d675e62e6501
royonem <roywmyung@gmail.com>
2026-06-17 17:01:22 +0900

    fix(call): populate clientId, translatorId, topicId in CallResponse from Mapper

--- src/main/java/com/roy/morago/mapper/CallMapper.java
+++ src/main/java/com/roy/morago/mapper/CallMapper.java
@@ -4,10 +4,14 @@ import com.roy.morago.dto.call.CallResponse;
 import com.roy.morago.dto.call.CallRequest;
 import com.roy.morago.entity.call.Call;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CallMapper {
+    @Mapping(target = "clientId", source = "client.id")
+    @Mapping(target = "translatorId", source = "translator.id")
+    @Mapping(target = "topicId", source = "topic.id")
     CallResponse createResponseFromEntity(Call call);
 
     Call createEntityFromRequest(CallRequest callRequest);
