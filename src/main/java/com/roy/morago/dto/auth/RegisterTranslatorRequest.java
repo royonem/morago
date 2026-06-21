dbb218530e60411c2df5e4c7d9bae1ccada78219
royonem <roywmyung@gmail.com>
2026-06-21 16:32:58 +0900

    refactor(dto): change register dtos to records

--- src/main/java/com/roy/morago/dto/auth/RegisterTranslatorRequest.java
+++ src/main/java/com/roy/morago/dto/auth/RegisterTranslatorRequest.java
@@ -2,37 +2,38 @@ package com.roy.morago.dto.auth;
 
 import com.roy.morago.enums.TopikLevel;
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class RegisterTranslatorRequest {
-    @NotBlank(message = "First name is required")
-    private String firstName;
-    @NotBlank(message = "Last name is required")
-    private String lastName;
-    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
-    @Pattern(
-            regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
-            message = "Password must contain at least one letter and one number"
-    )
-    @NotBlank(message = "Password is required")
-    private String password;
-    @NotBlank(message = "Please confirm your password")
-    private String confirmPassword;
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
-    private String email;
-    @NotBlank(message = "Phone number is required")
-    private String phone;
-    @NotNull(message = "TOPIK level is required")
-    private TopikLevel topikLevel;
-    @NotNull(message = "Birthdate is required")
-    private LocalDate birthdate;
+public record RegisterTranslatorRequest(
+        @NotBlank(message = "First name is required")
+        String firstName,
+
+        @NotBlank(message = "Last name is required")
+        String lastName,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
+                message = "Password must contain at least one letter and one number"
+        )
+        String password,
+
+        @NotBlank(message = "Please confirm your password")
+        String confirmPassword,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Phone number is required")
+        String phone,
+
+        @NotNull(message = "TOPIK level is required")
+        TopikLevel topikLevel,
+
+        @NotNull(message = "Birthdate is required")
+        LocalDate birthdate
+) {
 }
