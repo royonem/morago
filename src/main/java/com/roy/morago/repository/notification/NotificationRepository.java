54de8a057c0a21013ee91583f42aaa9b7315bab4
royonem <roywmyung@gmail.com>
2026-06-21 14:42:23 +0900

    feat(notification):
    - improve bulk transaction methods using repository
    - add pagination and filtering
    - organize annotation order in usercontroller

--- src/main/java/com/roy/morago/repository/notification/NotificationRepository.java
+++ src/main/java/com/roy/morago/repository/notification/NotificationRepository.java
@@ -1,13 +1,35 @@
 package com.roy.morago.repository.notification;
 
 import com.roy.morago.entity.notification.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByUserId(Long userId);
-    List<Notification> findByUserIdAndIsReadFalse(Long userId);
-
+public interface NotificationRepository extends JpaRepository<Notification, Long>, JpaSpecificationExecutor<Notification> {
+    Page<Notification> findByUserId(Long userId, Pageable pageable);
+    Page<Notification> findByUserIdAndIsReadFalse(Long userId, Pageable pageable);
+    void deleteByIdInAndUserId(List<Long> ids, Long userId);
+    void deleteByUserIdAndIsReadTrue(Long userId);
+    void deleteByIdInAndSentAtIsNull(List<Long> ids);
     Long countByUserIdAndIsReadFalse(Long userId);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = CASE WHEN n.isRead = true THEN false ELSE true END, " +
+            "n.readAt = CASE WHEN n.isRead = true THEN null ELSE CURRENT_TIMESTAMP END " +
+            "WHERE n.id IN :ids AND n.user.id = :userId")
+    void toggleReadByIdInAndUserId(@Param("ids") List<Long> ids, @Param("userId") Long userId);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = true, n.readAt = CURRENT_TIMESTAMP WHERE n.id IN :ids AND n.user.id = :userId")
+    void markAsReadByIdInAndUserId(List<Long> notificationIds, Long userId);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = true, n.readAt = CURRENT_TIMESTAMP WHERE n.user.id = :userId")
+    void markAsReadByUserId(Long userId);
 }
