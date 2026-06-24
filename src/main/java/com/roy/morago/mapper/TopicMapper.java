f2c8ee2ca6f8aeb0bd81182f2943271a9f2705c2
royonem <roywmyung@gmail.com>
2026-06-24 13:48:43 +0900

    fix(topic): fix topic to response id mapping

--- src/main/java/com/roy/morago/mapper/TopicMapper.java
+++ src/main/java/com/roy/morago/mapper/TopicMapper.java
@@ -4,10 +4,13 @@ import com.roy.morago.dto.topic.TopicRequest;
 import com.roy.morago.dto.topic.TopicResponse;
 import com.roy.morago.entity.topic.Topic;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TopicMapper {
+    @Mapping(target = "categoryId", source = "category.id")
+    @Mapping(target = "iconId", source = "icon.id")
     TopicResponse toResponse(Topic topic);
     Topic toEntity(TopicRequest request);
 }
